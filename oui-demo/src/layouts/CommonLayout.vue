eeb60041477da3c91d9dfeaff94845e96035d5f4
JC <13478707150@163.com>
2021-11-14 11:30:32 +0800

    fixe layout style

--- oui-demo/src/layouts/CommonLayout.vue
+++ oui-demo/src/layouts/CommonLayout.vue
@@ -22,8 +22,6 @@ export default {
 
 <style scoped lang="less">
 .common-layout{
-  display: flex;
-  flex-direction: column;
   min-width: 970px;
   height: 100vh;
   overflow: auto;
@@ -33,12 +31,13 @@ export default {
   background-position-x: center;
   background-position-y: 110px;
   background-size: 100%;
+  display: flex;
+  flex-direction: column;
   .content{
-    padding: 32px 0;
     flex: 1;
-    @media (min-width: 768px){
-      padding: 112px 0 24px;
-    }
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 }
 </style>
