e9b9232dcd862a61067c79285bae4f3175338771
13478707150@163.com <13478707150@163.com>
2021-11-23 19:48:39 +0800

    styles adjustment

--- oui-demo/src/main.js
+++ oui-demo/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import {initRouter} from './router'
 import './theme/index.less'
-import Oui from 'userty-design'
+import Usertyd from 'userty-design'
 import Viser from 'viser-vue'
 import '@/mock'
 import store from './store'
@@ -11,12 +11,12 @@ import Plugins from '@/plugins'
 import {initI18n} from '@/utils/i18n'
 import bootstrap from '@/bootstrap'
 import 'moment/locale/zh-cn'
-import 'userty-design/dist/oui.less';
+import 'userty-design/dist/usertyd.less';
 
 const router = initRouter(store.state.setting.asyncRoutes)
 const i18n = initI18n('CN', 'US')
 
-Vue.use(Oui)
+Vue.use(Usertyd)
 Vue.config.productionTip = false
 Vue.use(Viser)
 Vue.use(Plugins)
