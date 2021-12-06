142f84ac7fa7d5a959d81c93abdcc0a98d57d3ba
joge <jianghaoran@outlook.com>
2021-12-06 16:01:05 +0800

    project

--- example/src/config/default/index.js
+++
@@ -1,6 +0,0 @@
-const USERTYD = require('./userty.config')
-const ADMIN = require('./admin.config')
-const ANIMATE = require('./animate.config')
-const setting = require('./setting.config')
-
-module.exports = {USERTYD, ADMIN, ANIMATE, setting}
--- example/src/views/About.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <o-button>123</o-button>
-  </div>
-</template>
